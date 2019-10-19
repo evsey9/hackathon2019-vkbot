43209b04059bb453700b9d6e39aa6104b5007a7e
Evsey Antonovich <evseyru1@mail.ru>
2019-10-19 15:14:53 +0500

    removed id

--- admininterface.py
+++ admininterface.py
@@ -3,17 +3,17 @@ import PyQt5
 
 db = dataset.connect('sqlite:///botdatabase.db')
 
-table_teachers = db['teachers']
-table_groups = db['groups']
-table_addresses = db['addresses']
+teachers = db['teachers']
+groups = db['groups']
+addresses = db['addresses']
 
-table_teachers.insert(dict(id=1, lastname="Фамилия", firstname="Имя", middlename="Отчество",
+teachers.insert(dict(lastname="Фамилия", firstname="Имя", middlename="Отчество",
                            phone="89526664848", mail="mail@mail.ru", age=21))
 
-table_groups.insert(dict(id=1, location_id=1, day=1, time_start="10:00", time_end="18:00",
+groups.insert(dict(location_id=1, day=1, time_start="10:00", time_end="18:00",
                          days="1, 3", teacher=1))
 
-table_addresses.insert(dict(id=1, country="Россия", region="Тюменская область", city="Тюмень",
+addresses.insert(dict(country="Россия", region="Тюменская область", city="Тюмень",
                             street="улица Рижская", building="71", name="Гимназия №16", latitude=57.131320,
                             longitude=65.589212))
 
