939ac02d813ad46e3e214c972ffb0430e4dfbb9b
Evsey Antonovich <evseyru1@mail.ru>
2019-10-30 17:32:29 +0500

    Подправил кавычки, мелкий фикс

--- bot_basement.py
+++ bot_basement.py
@@ -8,20 +8,20 @@ from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 mysqlstr = ""
-with open("auth/mysqlauth.txt", 'r') as f:
+with open("auth/mysqlauth.txt", "r") as f:
     mysqlstr = f.read()
-db = dataset.connect('mysql://' + mysqlstr)
+db = dataset.connect("mysql://" + mysqlstr)
 
-teachers = db['teachers']
-groups = db['groups']
-location = db['locations']
+teachers = db["teachers"]
+groups = db["groups"]
+location = db["locations"]
 
 def main():
     session = requests.Session()
 
     # Авторизация пользователя:
     """
-    login, password = 'python@vk.com', 'mypassword'
+    login, password = "python@vk.com", "mypassword"
     vk_session = vk_api.VkApi(login, password)
 
     try:
@@ -34,7 +34,7 @@ def main():
     # Авторизация группы (для групп рекомендуется использовать VkBotLongPoll):
     # при передаче token вызывать vk_session.auth не нужно
     tokenstr = ""
-    with open('auth/vktoken.txt', 'r') as f:
+    with open("auth/vktoken.txt", "r") as f:
         tokenstr = f.read()
     vk_session = vk_api.VkApi(token=tokenstr)
 
@@ -49,32 +49,32 @@ def main():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             k = 0
             arguments = []
-            msgarr = event.text.split(' ')
+            msgarr = event.text.split(" ")
             if curcommand == "":
                 if msgarr[0].lower() in commands:
                     curcommand = msgarr[0].lower()
                     if len(msgarr) > 1:
-                        arguments = event.text[event.text.find(' ') + 1:].split('; ')
+                        arguments = event.text[event.text.find(" ") + 1:].split("; ")
                 else:
-                    msg = 'Я вас не понял, пожалуйста, повторите запрос.'
+                    msg = "Я вас не понял, пожалуйста, повторите запрос."
                     matches = difflib.get_close_matches(msgarr[0].lower(), commands, 1)
                     if matches:
-                        msg = 'Я вас не понял. Возможно, вы имели в виду ' + matches[0] + '?'
+                        msg = "Я вас не понял. Возможно, вы имели в виду " + matches[0] + "?"
                     vk.messages.send(  # Отправляем сообщение
                         user_id=event.user_id,
                         random_id=get_random_id(),
                         message=msg
                     )
             else:
-                arguments = event.text.split('; ')
-            if msgarr[0].lower() == 'назад':
+                arguments = event.text.split("; ")
+            if msgarr[0].lower() == "назад":
                 curcommand = ""
-            if curcommand == 'расписание':
+            if curcommand == "расписание":
                 if arguments == []:  # Если аргументов нет
                     vk.messages.send(  # Отправляем сообщение
                     user_id=event.user_id,
                     random_id=get_random_id(),
-                    message='Введите школу'
+                    message="Введите школу"
                     )
                 else:
                     print(arguments[0])
@@ -91,24 +91,22 @@ def main():
                                             "FROM groups AS g INNER JOIN locations school ON school.id = g.location_id \n"
                                             "INNER JOIN teachers teach ON teach.id = g.teacher ")
 
-                        timerows = db.create_table('timerows')
+                        timerows = db.create_table("timerows")
                         for i in loctable:
-                            if i['school_name'] == arguments[0]:
+                            if i["school_name"] == arguments[0]:
                                 timerows.insert(i)
                                 print(i)
-                        #loctable = db.
                         print(timerows.columns)
 
-                        #print(loctable)
                         weekdays = {
-                            '1': "Понедельник: ",
-                            '2': "Вторник: ",
-                            '3': "Среда: ",
-                            '4': "Четверг: ",
-                            '5': "Пятница: ",
-                            '6': "Суббота: ",
-                            '7': "Воскресенье: ",
-                            '8': "АА СТРАШНА ВЫРУБАЙ"
+                            "1": "Понедельник: ",
+                            "2": "Вторник: ",
+                            "3": "Среда: ",
+                            "4": "Четверг: ",
+                            "5": "Пятница: ",
+                            "6": "Суббота: ",
+                            "7": "Воскресенье: ",
+                            "8": "АА СТРАШНА ВЫРУБАЙ"
                         }
                         for i in range(1, 7):
                             result = db.query("SELECT * FROM timerows WHERE days LIKE '%" + str(i) + "%'")
@@ -116,26 +114,26 @@ def main():
                             result1 = []
                             for j in result:
                                 if j:
-                                    print('j : ', j)
+                                    print("j : ", j)
                                     didfind = True
                                     result1.append(j)
                             if didfind:
                                 msg.append(weekdays[str(i)])
                                 for j in result1:
                                     print(j)
-                                    time_start = j['time_start'].split(' ')[1]
-                                    time_end = j['time_end'].split(' ')[1]
-                                    startzrs = 1 if len(time_start.split(':')[1]) == 1 else 0
-                                    endzrs = 1 if len(time_end.split(':')[1]) == 1 else 0
-                                    time_start = time_start.split(':')[0] + ':' + '0' * startzrs + time_start.split(':')[1]
-                                    time_end = time_end.split(':')[0] + ':' + '0' * startzrs + time_end.split(':')[1]
-                                    msg.append(time_start + '-' + time_end + ' ' + j['last_name'] + ', ')
+                                    time_start = j["time_start"].split(" ")[1]
+                                    time_end = j["time_end"].split(" ")[1]
+                                    startzrs = 1 if len(time_start.split(":")[1]) == 1 else 0
+                                    endzrs = 1 if len(time_end.split(":")[1]) == 1 else 0
+                                    time_start = time_start.split(":")[0] + ":" + "0" * startzrs + time_start.split(":")[1]
+                                    time_end = time_end.split(":")[0] + ":" + "0" * endzrs + time_end.split(":")[1]
+                                    msg.append(time_start + "-" + time_end + " " + j["last_name"] + ", ")
                         print(msg)
-                        db['timerows'].drop()
+                        db["timerows"].drop()
                         vk.messages.send(  # Отправляем сообщение
                             user_id=event.user_id,
                             random_id=get_random_id(),
-                            message=''.join(msg)
+                            message="".join(msg)
                         )
                     else:
                         vk.messages.send(  # Отправляем сообщение
@@ -143,23 +141,23 @@ def main():
                             random_id=get_random_id(),
                             message="Школа не найдена. Попробуйте обратиться к списку адресов."
                         )
-            elif event.text == 'расписание':  # Если написали заданную фразу
+            elif curcommand == "расписание":  # Если написали заданную фразу
                 k = 0
                 vk.messages.send(  # Отправляем сообщение
                 user_id=event.user_id,
                 random_id=get_random_id(),
-                message='5'
+                message="5"
                 )
-            elif event.text == 'школа':  # Если написали заданную фразу
+                curcommand = ""
+            elif curcommand == "школа":  # Если написали заданную фразу
                 k = 0
                 vk.messages.send(  # Отправляем сообщение
                 user_id=event.user_id,
                 random_id=get_random_id(),
-                message='5'
+                message="5"
                 )
-            elif event.text == 'назад':  # Если написали заданную фразу
-                k = 1
+                curcommand = ""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
